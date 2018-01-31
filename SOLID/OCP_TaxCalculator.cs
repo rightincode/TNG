abaede50e0c4d5b4cd05e43410d1125184f16003
Richard Taylor <rtaylor@rightincode.com>
2018-01-30 21:21:59 -0500

    Removed unneeded wrapper class around taxcalculatorbase and its subclasses

--- SOLID/OCP_TaxCalculator.cs
+++ SOLID/OCP_TaxCalculator.cs
@@ -6,39 +6,36 @@ using System.Threading.Tasks;
 
 namespace SOLID
 {
-    class OCP_TaxCalculator
+    public abstract class TaxCalculatorBase
     {
-        public abstract class TaxCalculatorBase
-        {
-            public decimal TotalAmount { get; set; }
-            public abstract decimal CalculateTax();
-        }
+        public decimal TotalAmount { get; set; }
+        public abstract decimal CalculateTax();
+    }
 
-        public class USATax : TaxCalculatorBase
+    public class USATax : TaxCalculatorBase
+    {
+        public override decimal CalculateTax()
         {
-            public override decimal CalculateTax()
-            {
-                //calculate tax as per USA rules
-                return 0;
-            }
+            //calculate tax as per USA rules
+            return 0;
         }
+    }
 
-        public class UKTax : TaxCalculatorBase
+    public class UKTax : TaxCalculatorBase
+    {
+        public override decimal CalculateTax()
         {
-            public override decimal CalculateTax()
-            {
-                //calculate tax as per UK rules
-                return 0;
-            }
+            //calculate tax as per UK rules
+            return 0;
         }
+    }
 
-        public class IndiaTax : TaxCalculatorBase
+    public class IndiaTax : TaxCalculatorBase
+    {
+        public override decimal CalculateTax()
         {
-            public override decimal CalculateTax()
-            {
-                //calculate tax as per India rules
-                return 0;
-            }
+            //calculate tax as per India rules
+            return 0;
         }
     }
 }
