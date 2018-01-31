5659aec8234cc2c751c74c8abfa6ea874af668b1
Richard Taylor <rtaylor@rightincode.com>
2018-01-30 22:43:35 -0500

    Reorganized SRP code in solution

---
+++ SOLID/SRP/Compliant/SR_Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOLID.SRP.Compliant
+{
+    public class SR_Logger
+    {
+        public void LogIntResult(int result)
+        {
+            Console.WriteLine("The result of the operation is " + result.ToString());
+        }
+    }
+}
