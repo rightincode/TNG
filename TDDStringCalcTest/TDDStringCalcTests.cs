c7d70959060b370ab3ff220d1afd879340d3a20e
Richard Taylor <rltaylor@sentryone.com>
2018-02-06 15:24:39 -0500

    Started on TDD calculator

---
+++ TDDStringCalcTest/TDDStringCalcTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TDDStringCalcTest
+{
+    [TestClass]
+    public class TDDStringCalcTests
+    {
+        [TestMethod]
+        public void Addition()
+        {
+            var calculator = new Calculator()
+        }
+    }
+}
